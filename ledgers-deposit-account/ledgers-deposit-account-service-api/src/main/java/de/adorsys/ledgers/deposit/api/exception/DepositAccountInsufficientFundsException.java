1e1c7c7687e6bcb3ffab7ff4062ab46187875b65
Dmitry Mishchuk <dmi@adorsys.com.ua>
2019-06-14 14:17:48 +0300

    fixed MR discussions

--- ledgers-deposit-account/ledgers-deposit-account-service-api/src/main/java/de/adorsys/ledgers/deposit/api/exception/DepositAccountInsufficientFundsException.java
+++ ledgers-deposit-account/ledgers-deposit-account-service-api/src/main/java/de/adorsys/ledgers/deposit/api/exception/DepositAccountInsufficientFundsException.java
@@ -1,17 +1,9 @@
 package de.adorsys.ledgers.deposit.api.exception;
 
-import de.adorsys.ledgers.deposit.api.domain.PaymentBO;
-
 public class DepositAccountInsufficientFundsException extends RuntimeException {
     private static final String MESSAGE = "Payment with id=%s rejected due to insufficient funds";
-    private PaymentBO payment;
 
-    public DepositAccountInsufficientFundsException(PaymentBO payment) {
-        super(String.format(MESSAGE, payment.getPaymentId()));
-        this.payment=payment;
-    }
-
-    public PaymentBO getPayment() {
-        return payment;
+    public DepositAccountInsufficientFundsException(String paymentId) {
+        super(String.format(MESSAGE, paymentId));
     }
 }
