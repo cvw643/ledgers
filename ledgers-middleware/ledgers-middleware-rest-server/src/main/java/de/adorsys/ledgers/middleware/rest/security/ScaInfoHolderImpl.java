783a2a50df4ad5163ecb0de5f4dba2e126471b16
Petro Rudenko <pru@adorsys.com.ua>
2019-07-08 14:01:48 +0300

    rename auth facade

--- ledgers-middleware/ledgers-middleware-rest-server/src/main/java/de/adorsys/ledgers/middleware/rest/security/ScaInfoHolderImpl.java
+++ ledgers-middleware/ledgers-middleware-rest-server/src/main/java/de/adorsys/ledgers/middleware/rest/security/ScaInfoHolderImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class AuthenticationFacadeImpl implements AuthenticationFacade {
+public class ScaInfoHolderImpl implements ScaInfoHolder {
     private final AccessTokenTO accessTokenTO;
 
     @Override
