c7c40fb75966dba91bdea05232fb084509900ad5
olhaz <oza@adorsys.com.ua>
2019-10-02 18:31:39 +0300

    fix triggers periodic payment too early

--- ledgers-deposit-account/ledgers-deposit-account-service-impl/src/main/java/de/adorsys/ledgers/deposit/api/service/impl/ExecutionTimeHolder.java
+++ ledgers-deposit-account/ledgers-deposit-account-service-impl/src/main/java/de/adorsys/ledgers/deposit/api/service/impl/ExecutionTimeHolder.java
@@ -30,13 +30,14 @@ public class ExecutionTimeHolder {
 
     public static LocalDate getExecutionDate(Payment payment) {
         LocalDate nextExecution = holder.get(payment.getFrequency()).apply(payment);
-        return payment.getDayOfExecution() == null
+        return payment.getDayOfExecution() == null || payment.getFrequency().equals(FrequencyCode.DAILY) ||
+                       payment.getFrequency().equals(FrequencyCode.WEEKLY) || payment.getFrequency().equals(FrequencyCode.EVERYTWOWEEKS)
                        ? nextExecution
                        : LocalDate.of(nextExecution.getYear(), nextExecution.getMonth(), payment.getDayOfExecution());
     }
 
     private static LocalDate getDateForWeeks(Payment payment, int weeksToAdd) {
-        return payment.getStartDate().plusWeeks(getWeeksDifference(payment) + weeksToAdd);
+            return payment.getExecutedDate().toLocalDate().plusWeeks(weeksToAdd);
     }
 
     private static LocalDate getDateForMonths(Payment payment, int monthsToAdd) {
@@ -44,10 +45,6 @@ public class ExecutionTimeHolder {
     }
 
     private static long getMonthDifference(Payment payment) {
-        return ChronoUnit.MONTHS.between(payment.getExecutedDate().toLocalDate(), payment.getStartDate());
-    }
-
-    private static long getWeeksDifference(Payment payment) {
-        return ChronoUnit.WEEKS.between(payment.getExecutedDate().toLocalDate(), payment.getStartDate());
+        return ChronoUnit.MONTHS.between(payment.getStartDate(), payment.getExecutedDate().toLocalDate());
     }
 }
